9dfa08885b44cf38fb27f61edf8b710a08fcb1de
John <degjohn1@gmail.com>
2025-09-06 15:52:40 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed= new Typed(".text",{
-    strings:["Sytem & Network Administrator" , "web developper"],
+    strings:["System & Network Administrator" , "web developper"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
